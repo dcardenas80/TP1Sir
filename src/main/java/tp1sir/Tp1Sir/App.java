82bd5519b5ef7a1a27cd86cf95fae2e6e7e1a62f
Diego <dym2312@hotmail.com>
2018-01-09 10:20:19 +0100

    Tp1Sir

---
+++ src/main/java/tp1sir/Tp1Sir/App.java
@@ -0,0 +1,13 @@
+package tp1sir.Tp1Sir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
